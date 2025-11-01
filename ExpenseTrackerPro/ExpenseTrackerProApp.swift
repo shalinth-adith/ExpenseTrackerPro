626142c702519a861f3dcfae5c87dcfc6e1a2b5c
shalinth-adith <shalinth.adith@outlook.com>
2025-11-01 11:27:54 +0530

    Initial Commit

---
+++ ExpenseTrackerPro/ExpenseTrackerProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerProApp.swift
+//  ExpenseTrackerPro
+//
+//  Created by shalinth adithyan on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTrackerProTests/ExpenseTrackerProTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerProTests.swift
+//  ExpenseTrackerProTests
+//
+//  Created by shalinth adithyan on 01/11/25.
+//
+
+import Testing
+@testable import ExpenseTrackerPro
+
+struct ExpenseTrackerProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
