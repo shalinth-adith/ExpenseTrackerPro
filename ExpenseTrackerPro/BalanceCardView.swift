44fd330bd44f5913e5c97335173368e21939f64e
shalinth-adith <shalinth.adith@outlook.com>
2025-11-01 14:26:41 +0530

    Main View

---
+++ ExpenseTrackerPro/BalanceCardView.swift
@@ -0,0 +1,18 @@
+//
+//  BalanceCardView.swift
+//  ExpenseTrackerPro
+//
+//  Created by shalinth adithyan on 01/11/25.
+//
+
+import SwiftUI
+
+struct BalanceCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BalanceCardView()
+}
